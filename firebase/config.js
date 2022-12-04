7879948f99d970bfa6d4c583d638287dbd2e4e0e
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-04 15:38:45 -0300

    integrando firebase

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCma2LHjyZPGoelTblV9mKy-_FD4iX58GE",
+  authDomain: "photograpys.firebaseapp.com",
+  projectId: "photograpys",
+  storageBucket: "photograpys.appspot.com",
+  messagingSenderId: "448107579466",
+  appId: "1:448107579466:web:d9268c8c695d68d68d7eb3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
