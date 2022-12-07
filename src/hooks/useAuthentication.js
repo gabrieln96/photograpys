0ec75ab60509ebab32259d9349297244b08d4f14
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-06 23:43:11 -0300

    auth de login

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -79,18 +79,48 @@ export const useAuthentication = () => {
         checkIfIsCancelled();
 
         signOut(auth)
+    };
 
+    // login 
+
+    const login = async(data) => {
+
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            setLoading(false);
+            
+        } catch (error) {
+            let systemErrorMessage;
+
+            if (error.message.includes("user-not-found")){
+                systemErrorMessage = "Usuário não encontrado!"
+            } else if (error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta!"
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente novamente"
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
     }
 
     useEffect(() => {
         return () => setCancelled(true);
     }, []);
 
-    return{
+    return {
         auth,
         createUser,
         error,
         loading,
         logout,
+        login,
     };
 };
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -5,11 +5,11 @@ import { useAuthentication } from '../../hooks/useAuthentication'
 
 const Login = () => {
 
-     const [email, setEmail] = useState ("")
+    const [email, setEmail] = useState ("")
     const [password, setPassword] = useState ("")
     const [error, setError] = useState("")
   
-    const {createUser, error: authError, loading} = useAuthentication();
+    const {login, error: authError, loading} = useAuthentication();
     
     const handleSubmit = async (e) => {
       e.preventDefault()
@@ -21,7 +21,7 @@ const Login = () => {
         password
       }
   
-      const res = await createUser(user)
+      const res = await login(user)
   
       console.log(res);
     };
