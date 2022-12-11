26779fc33f14af85d8dbc04e9d228f40facd3c38
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-11 14:52:06 -0300

    estrutura do dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -13,6 +13,12 @@ const Dashboard = () => {
 
   const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => { };
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
         <h2>Dashboard</h2>
@@ -23,14 +29,29 @@ const Dashboard = () => {
             <Link to='/posts/create' className='btn'>Criar primeiro posts</Link>
           </div>
         ) : (
+          <>
           <div>
-            <p>Tem posts!</p>
+            <span>Titulo</span>
+            <span>Ações</span>
           </div>
+           {posts && posts.map((post) => <div key={post.id}>
+          <p>{post.title}</p>
+          <div>
+            <Link to={`/posts/${post.id}`} className="btn btn-outline">
+              Ver
+            </Link>
+            <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+              Editar
+            </Link>
+            <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">
+              Exluir
+            </button>
+          </div>
+           </div>)}
+          </>
         )}
 
-        {posts && posts.map((post) => (
-          <h3>{post.title}</h3>
-        ))}
+       
     </div>
   )
 }
