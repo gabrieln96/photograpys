cd29e98ba28413fbda1aeb3cd62b320d6efb53da
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-11 14:27:18 -0300

    estruturando dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,32 @@
 import styles from './Dashboard.module.css'
 
+import {Link} from 'react-router-dom'
+
+import {useAuthValue} from '../../context/AuthContext'
+import {useFetchDocuments} from '../../hooks/useFetchDocuments'
+
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  // post do user
+
+  const posts = []
+
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie os seus posts</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.noposts}>
+            <p>Não foram encontratos posts</p>
+            <Link to='/posts/create' className='btn'>Criar primeiro posts</Link>
+          </div>
+        ) : (
+          <div>
+            <p>Tem posts!</p>
+          </div>
+        ) }
     </div>
   )
 }
