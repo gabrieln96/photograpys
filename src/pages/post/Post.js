6e216f8b26aa419b25e9e45a3632ceac60ccb1c8
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-11 14:15:33 -0300

    css da pagina de post

--- src/pages/post/Post.js
+++ src/pages/post/Post.js
@@ -7,11 +7,21 @@ const Post = () => {
     const {id} = useParams();
     const {document: post, loading} = useFetchDocument("posts", id)
   return (
-    <div>
+    <div className={styles.post_container}>
         {loading && <p>Carregando post...</p>}
         {post && (
             <>
             <h1>{post.title}</h1>
+            <img src={post.image} alt={post.title} />
+            <p>{post.body}</p>
+            <h3>Esto post trata sobre:</h3>
+            <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+                <p key={tag}>
+                <span>#</span>
+                {tag}</p>
+            ))}
+            </div>
             </>
         )}
     </div>
